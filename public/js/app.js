8db32312344b8c80b570978674744e8e9f741efe
Calum Towers <me@calumtowers.com>
2019-10-15 21:32:08 +0100

    Small styling changes from npm running

--- public/js/app.js
+++ public/js/app.js
@@ -19287,8 +19287,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/calumtowers/Code/vatsim-atis/Website/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/calumtowers/Code/vatsim-atis/Website/resources/sass/sb-admin-2.scss */"./resources/sass/sb-admin-2.scss");
+__webpack_require__(/*! /Users/calumtowers/Code/vatsim-atis/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Users/calumtowers/Code/vatsim-atis/resources/sass/sb-admin-2.scss */"./resources/sass/sb-admin-2.scss");
 
 
 /***/ })
